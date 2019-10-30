7b70a613869a22e078c01d1b0e25cc7731db26f0
NikitaFeodonit <nfeodonit@yandex.com>
2019-10-31 00:46:32 +0300

    Cairo_CMake_Files: Fix macOS build in FindLibM.cmake, do not find includes for libm

--- patch/Cairo_CMake_Files/cmake/FindLibM.cmake
+++ patch/Cairo_CMake_Files/cmake/FindLibM.cmake
@@ -17,28 +17,22 @@
 #
 # ::
 #
-#   LIBM_INCLUDE_DIRS  - where to find math.h, etc.
 #   LIBM_LIBRARIES     - List of libraries when using libm.
 #   LIBM_FOUND         - True if math library found.
-
-find_path(LIBM_INCLUDE_DIRS
-  NAMES math.h
-)
 
 find_library(LIBM_LIBRARIES
   NAMES m
 )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LibM DEFAULT_MSG LIBM_LIBRARIES LIBM_INCLUDE_DIRS)
-mark_as_advanced(LIBM_INCLUDE_DIRS LIBM_LIBRARIES)
+find_package_handle_standard_args(LibM DEFAULT_MSG LIBM_LIBRARIES)
+mark_as_advanced(LIBM_LIBRARIES)
 
 if(LIBM_FOUND)
   if(NOT TARGET LIBM::LIBM)
     add_library(LIBM::LIBM UNKNOWN IMPORTED)
     set_target_properties(LIBM::LIBM PROPERTIES
       IMPORTED_LOCATION "${LIBM_LIBRARIES}"
-      INTERFACE_INCLUDE_DIRECTORIES "${LIBM_INCLUDE_DIRS}"
     )
   endif()
 endif()
